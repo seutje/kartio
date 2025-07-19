0375f88a2b8867b154436c2f7572570a639e20a3
Steve De Jonghe <seutje@gmail.com>
2025-07-19 09:20:44 +0200

    Stop kart and grant invulnerability on projectile hit

--- tests/Missile.test.js
+++ tests/Missile.test.js
@@ -1,4 +1,6 @@
 const { Track } = require('../src/js/Track');
+const { Kart } = require('../src/js/Kart');
+const { Missile, Mine } = require('../src/js/Powerup');
 
 describe('Missile integration', () => {
     test('track update should advance missiles', () => {
@@ -8,5 +10,33 @@ describe('Missile integration', () => {
         track.missiles.push(missile);
         track.update(0.1);
         expect(missile.update).toHaveBeenCalled();
+    });
+});
+
+describe('Projectile hits', () => {
+    test('missile hit stops kart and grants invulnerability', () => {
+        const scene = { add: jest.fn(), remove: jest.fn() };
+        const kart = new Kart(0xff0000, scene);
+        window.gameEngine = { karts: [kart] };
+        const missile = new Missile(kart.position.clone(), 0, scene);
+        missile.owner = {};
+        missile.checkCollisions();
+        expect(kart.isInvulnerable).toBe(true);
+        expect(kart.invulnerabilityTime).toBe(3);
+        expect(kart.velocity.length()).toBe(0);
+        expect(missile.active).toBe(false);
+    });
+
+    test('mine hit stops kart and grants invulnerability', () => {
+        const scene = { add: jest.fn(), remove: jest.fn() };
+        const kart = new Kart(0xff0000, scene);
+        window.gameEngine = { karts: [kart] };
+        const mine = new Mine(kart.position.clone(), scene);
+        mine.owner = {};
+        mine.checkCollisions();
+        expect(kart.isInvulnerable).toBe(true);
+        expect(kart.invulnerabilityTime).toBe(3);
+        expect(kart.velocity.length()).toBe(0);
+        expect(mine.active).toBe(false);
     });
 });
