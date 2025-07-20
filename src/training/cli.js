d5551ce13a9bce0cb1f6aaac3923b5d91195f3f8
Steve De Jonghe <seutje@gmail.com>
2025-07-20 09:41:29 +0200

    Add powerup sensor and output

--- src/training/cli.js
+++ src/training/cli.js
@@ -136,7 +136,7 @@ class TrainingEnvironment {
     
     initializePopulation() {
         for (let i = 0; i < this.populationSize; i++) {
-            const network = new NeuralNetwork(9, 10, 2);
+            const network = new NeuralNetwork(10, 10, 3);
             this.population.push({
                 network: network,
                 fitness: 0,
